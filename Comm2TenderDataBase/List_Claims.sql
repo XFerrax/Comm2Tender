09479ca1bd173547044bff37b7cb5501350921a5
Artem <ArtDantes@yandex.ru>
2024-05-20 21:56:23 +0300

    rebuild datatable

--- Comm2TenderDataBase/List_Claims.sql
+++ Comm2TenderDataBase/List_Claims.sql
@@ -1,8 +1,9 @@
 ﻿create table dbo.List_Claims (
-   ID                   int                  not null,
+   ID                   int                  identity,
    ID_Pos_Tender        int                  null,
    ID_Claim             int                  null,
    Is_F_Maejuere        bit                  null,
+   Date_Correction      date                 null,
    constraint PK_LIST_CLAIMS primary key (ID), 
     CONSTRAINT [FK_LIST_CLA_REFERENCE_POS_TEND] FOREIGN KEY (ID_Pos_Tender) REFERENCES Pos_Tender(Id), 
     CONSTRAINT [FK_LIST_CLA_REFERENCE_DICT_CLA] FOREIGN KEY (ID_Claim) REFERENCES Dict_Claims(Id)
