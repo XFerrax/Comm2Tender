d13f78e5566ed1f1d1dbf3b26af91d42a93edde0
Artem <ArtDantes@yandex.ru>
2024-05-21 08:46:37 +0300

    Added many tables

--- Comm2TenderDataBase/Var_Contragent_Of_Tenders.sql
+++ Comm2TenderDataBase/Var_Contragent_Of_Tenders.sql
@@ -7,5 +7,9 @@
    ID_Сustoms_duty      int                  null,
    ID_Economic_effect   int                  identity,
    Reliability_Assessment float                null,
+   Count_Pos int null,
+   Position_Price float null,
+   Delivery_Cost float null,
+   Count_Day_Delivery int null,
    constraint PK_Var_Contragent_Of_Tenders primary key (ID_Economic_effect)
 )
